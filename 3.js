b18b29be19a903b59858df5fe18003082dfdc94c
Ricsi <bakonyi.richard1213@gmail.com>
2020-09-07 16:20:21 +0200

    2 gumicukor

---
+++ 3.js
@@ -0,0 +1,20 @@
+const getInput = require("ricsi-getinput");
+
+async function main()
+{
+    /*let Elsoszam = Number (await getInput("kérlek adj meg egy számot"));
+    let Masodikszam = Number (await getInput("kérlek adj meg egy számot"));
+    let Harmadikszam = Number (await getInput("kérlek adj meg egy számot"));
+    let Negyedikszam = Number (await getInput("kérlek adj meg egy számot"));
+    let Otodikszam = Number (await getInput("kérlek adj meg egy számot"));
+    console.log(Elsoszam + Masodikszam + Harmadikszam + Negyedikszam + Otodikszam);
+    console.log()*/
+    let Elsoszam = 4;
+    let Masodikszam = 5; 
+    let Harmadikszam = 6;
+    let Negyedikszam = 2;
+    let Otodikszam = 2;
+    let Vegeredmeny = Elsoszam + Masodikszam + Harmadikszam + Negyedikszam + Otodikszam;
+     console.log(Elsoszam + " + " +  Masodikszam +  " + " +  Harmadikszam + " + " +  Negyedikszam +  " + " +  Otodikszam + " = " +  Vegeredmeny);
+}
+main();
